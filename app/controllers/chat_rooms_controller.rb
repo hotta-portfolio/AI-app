91185daad16fcab4ab04021f2f4c0c7eb57f5c98
hotta-portfolio <hotta@hottaryoutarounoMacBook-Pro.local>
2025-06-15 02:19:31 +0900

    #29 チャットルームの実装

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,7 +1,26 @@
 class ChatRoomsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_chat_room
-  before_action :authorize_user!
+  before_action :set_chat_room, only: [:show]
+  before_action :authorize_user!, only: [:show]
+
+  def index
+    case params[:role]
+    when 'buyer'
+      @chat_rooms = ChatRoom.joins(:purchase)
+                            .where(purchases: { user_id: current_user.id })
+                            .includes(:knowhow)
+    when 'seller'
+      @chat_rooms = ChatRoom.joins(:knowhow)
+                            .where(knowhows: { user_id: current_user.id })
+                            .includes(:purchase)
+    else
+      # 両方まとめて表示も可能（購入者＋販売者両方のchat_roomを取得）
+      @chat_rooms = ChatRoom.joins(:purchase, :knowhow)
+                            .where('purchases.user_id = :user_id OR knowhows.user_id = :user_id', user_id: current_user.id)
+                            .distinct
+                            .includes(:purchase, :knowhow)
+    end
+  end
 
   def show
     @message = Message.new
