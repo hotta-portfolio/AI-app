17cd187db5976eebab1d46b8c608dc091077fa59
Hideyuki Matsuyama <choripon@gmail.com>
2025-06-21 03:00:07 +0900

    Ruby7.2から廃止になったenumの構文をチェックするetc

--- spec/models/knowhow_spec.rb
+++ spec/models/knowhow_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Knowhow, type: :model do
 
     let(:knowhow) { build(:knowhow, user:, tag_list: tag_list) }
     let(:user) { create(:user) }
-    let(:tag_list) { [] }
+    let(:tag_list) { '' }
 
     it { is_expected.to be_truthy }
 
@@ -16,6 +16,18 @@ RSpec.describe Knowhow, type: :model do
       it '引数の tag_list に該当する Tag が登録される' do
         subject
         expect(knowhow.tags.pluck(:name)).to match_array(%w[タグ_A タグ_B])
+      end
+
+      context 'tag_list が nil' do
+        let(:tag_list) { nil }
+
+        it { is_expected.to be_truthy }
+      end
+
+      context 'tag_list が 数値' do
+        let(:tag_list) { 1 }
+
+        it { expect { subject }.to raise_error(NoMethodError) }
       end
     end
   end
